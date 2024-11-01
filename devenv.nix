0ce6aa1fb38265d4e09fd0db36750efff800384f
Eastern-Dream <eyrie@posteo.net>
2024-11-01 16:13:22 -0400

    fix devenv

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,13 @@
-{ pkgs, lib, config, inputs, ... }:
+{ pkgs, lib, ... }:
 
 {
   android = {
     enable = true;
     flutter.enable = true;
     android-studio.enable = true;
+    buildTools.version = [ "34.0.0" ];
+    platforms.version = [ "33" "34" ];
   };
+  
+  languages.java.jdk.package = lib.mkForce pkgs.jdk17;
 }
