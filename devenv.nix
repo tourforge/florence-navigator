a42d83f4d96d66089b314aa5b8b4d5d5017dd98a
Eastern-Dream <eyrie@posteo.net>
2025-05-22 14:43:36 -0400

    update deps and environment

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,12 @@
-{ pkgs, lib, ... }:
-
+{ pkgs, inputs, lib, ... }:
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+in
 {
   android = {
     enable = true;
     flutter.enable = true;
+    flutter.package = pkgs-unstable.flutter;
     android-studio.enable = true;
     buildTools.version = [ "34.0.0" ];
     platforms.version = [ "33" "34" ];
