554774bb714eac0f2942ff0f788d0ae3460ec02d
Eastern-Dream <Eastern-Dream@users.noreply.github.com>
2025-12-19 13:13:23 -0500

    upgrade build

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,22 @@
-{ pkgs, inputs, lib, ... }:
-let
-  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
-in
+{ config, pkgs, lib, ... }:
+
 {
   android = {
     enable = true;
     flutter.enable = true;
-    flutter.package = pkgs-unstable.flutter;
-    android-studio.enable = true;
-    buildTools.version = [ "34.0.0" ];
-    platforms.version = [ "33" "34" ];
+
+    buildTools.version = [ "35.0.0" "36.0.0" ];
+    platforms.version = [ "35" "36" ];
+    
+    ndk.enable = true;
+    ndk.version = [ "29.0.14206865" ];
   };
-  
-  languages.java.jdk.package = lib.mkForce pkgs.jdk17;
+
+  languages.java = {
+    jdk.package = lib.mkForce pkgs.jdk17;
+  };
+
+  packages = with pkgs; [
+    bundletool
+  ];
 }
