5cead1c0f2392065d1b948c31075b84e4fd3bd2d
Eastern-Dream <Eastern-Dream@users.noreply.github.com>
2025-12-23 11:54:56 -0500

    new build for 16kb alignment

--- devenv.nix
+++ devenv.nix
@@ -1,15 +1,24 @@
 { config, pkgs, lib, ... }:
 
 {
+  languages.kotlin.enable = true;
+
   android = {
     enable = true;
     flutter.enable = true;
-
-    buildTools.version = [ "35.0.0" "36.0.0" ];
-    platforms.version = [ "35" "36" ];
+    
+    buildTools.version = [
+      # This one is for zipalign for ELF alignment checking
+      "35.0.0-rc3"
+      "35.0.0"
+    ];
+    platforms.version = [ "33" "34" "35" "36" ];
     
     ndk.enable = true;
     ndk.version = [ "29.0.14206865" ];
+
+    android-studio.enable = true;
+    emulator.enable = true;
   };
 
   languages.java = {
@@ -18,5 +27,17 @@
 
   packages = with pkgs; [
     bundletool
+    gemini-cli-bin
   ];
+
+  tasks."android-build:build-apk" = {
+    exec = "flutter clean && flutter build apk";
+    cwd = "${config.env.DEVENV_ROOT}";
+  };
+
+  tasks."android-build:check-elf-alignment" = {
+    exec = "./check_elf_alignment.sh build/app/outputs/flutter-apk/app-release.apk";
+    cwd = "${config.env.DEVENV_ROOT}";
+    after = [ "android-build:build-apk" ];
+  };
 }
